a60c4208a4cab9bb5575cb7dd80a736ef79ea2cd
Grigori Koblitskiy <koblicky.g.a@effective-group.ru>
2026-04-08 16:45:56 +0300

    add(deptrac, clear arch)

--- src/Framework/Infrastructure/Kernel.php
+++ src/Framework/Infrastructure/Kernel.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace App\Framework\Infrastructure;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
--- src/Music/Infrastructure/Controller/HomeController.php
+++ src/Music/Infrastructure/Controller/HomeController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller;
+namespace App\Music\Infrastructure\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
